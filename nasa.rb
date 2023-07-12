16a8f7dee50d3f30c1597e6dee3371a0b832021e
Alejandro Piñango <36819641+apinango70@users.noreply.github.com>
2023-07-11 22:19:47 -0400

    Update nasa.rb
    Actualización de codigo ruby

--- nasa.rb
+++ nasa.rb
@@ -33,22 +33,20 @@ def build_web_page(data)
   output += "<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css' integrity='sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO' crossorigin='anonymous'>\n"
   output += "<title>Fotos API Nasa</title>\n"
   output += "</head>\n"
-  
   output += "<body>\n"
   output += "<table class='table'>\n"
   output += "<style>img { width: 400px; height: 400px; }</style>\n"
-  
-	output += "<h1>Fotos de Marte tomadas por la sonda Curiosity</h1>\n"
-	output += "<div class='container'>\n"
-	output += "<div class='row justify-content-center'>\n"
-	output += "<div class='row'>\n"
+  output += "<h1>Fotos de Marte tomadas por la sonda Curiosity</h1>\n"
+  output += "<div class='container'>\n"
+  output += "<div class='row justify-content-center'>\n"
+  output += "<div class='row'>\n"
   
   #Guardo 2 imágenes por columna (2)
   photos.each_slice(2) do |row_photos|  
     output += "\n\t<tr>"
 
-    #Itero sobre las fotos
-    row_photos.each do |photo|  
+  #Itero sobre las fotos
+  row_photos.each do |photo|  
       #Agrego cada imagen descargada en una celda
       output += "\n\t\t<td><img src='#{photo}'></td>"  
     end
@@ -58,7 +56,7 @@ def build_web_page(data)
   output += "\n</table>\n"
   output += "</div>\n</div>\n</div>\n</body>\n</html>"
 
-  #Escribo la tabla HTML con os datos recogidos arriba 
+  #Escribo el archivo html con los datos definidos más arriba 
   File.write("fotos_nasa.html", output)  
 end
 
@@ -73,5 +71,5 @@ def photos_count(data)
   data["photos"].map { |x| x["camera"]["name"] }.group_by { |x| x }.map { |k, v| [k, v.count] }
 end
 
-#puts photos_count(data)  
+pp photos_count(data)  
 puts "El html fuecreado con éxito"
